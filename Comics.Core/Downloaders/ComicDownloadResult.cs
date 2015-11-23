510967fa067bf47ccd891814c62c1a522f7539fc
Matt Burke <mburke@nerdery.com>
2015-11-23 07:13:23 -0600

    move download result to its own file

---
+++ Comics.Core/Downloaders/ComicDownloadResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Comics.Core.Downloaders
+{
+    public struct ComicDownloadResult
+    {
+        public int StatusCode { get; }
+        public string Content { get; }
+        public bool NotFound => StatusCode == 404;
+
+        public ComicDownloadResult(int statusCode, string content)
+        {
+            StatusCode = statusCode;
+            Content = content;
+        }
+
+    }
+}
--- Comics.Core/Downloaders/ExplosmDownloader.cs
+++ Comics.Core/Downloaders/ExplosmDownloader.cs
@@ -1,29 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
-
-using Comics.Core.Parsers;
 
 namespace Comics.Core.Downloaders
 {
     public interface IExplosmWebClient
     {
         ComicDownloadResult GetComicHtml(int comicNumber);
-    }
-
-    public struct ComicDownloadResult
-    {
-        public int StatusCode { get; }
-        public string Content { get; }
-        public bool NotFound => StatusCode == 404;
-
-        public ComicDownloadResult(int statusCode, string content)
-        {
-            StatusCode = statusCode;
-            Content = content;
-        }
-
     }
 
     // ReSharper disable once ClassNeverInstantiated.Global
