f5a3944a495c3eaf171e332ec88c81f5d73cb4bf
Matt Burke <mburke@nerdery.com>
2015-11-23 16:37:19 -0600

    stub out job runner to be a webjob

---
+++ Comics.JobRunner/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Comics.Core.Downloaders;
+using Comics.Core.Persistence;
+
+namespace Comics.JobRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new ComicsContext())
+            {
+                var repository = new ComicsRepository(context);
+                var client = new ExplosmWebClient();
+
+                var importer = new ExplosmImporter(repository, client);
+                importer.ImportNewComics(4120);
+            }
+        }
+    }
+}
