dc557b7dceb3dffe71c4a7868f1315c70faec3a3
Matt Burke <mburke@nerdery.com>
2015-11-24 11:28:37 -0600

    parse dilbert fixture

---
+++ Comics.Core/Parsers/DilbertParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using HtmlAgilityPack;
+
+namespace Comics.Core.Parsers
+{
+    public static class DilbertParser
+    {
+        public static ComicParseResult Parse(string html)
+        {
+            if (html == null) throw new ArgumentNullException(nameof(html));
+            if (html == "") throw new ArgumentException("html must have some contents", nameof(html));
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            var img = doc.QuerySelector(".img-comic");
+            var src = img?.GetAttributeValue("src", null);
+
+            var meta = doc.QuerySelector("meta[property='article:publish_date']");
+            var date = meta?.GetAttributeValue("content", null);
+
+            if (src == null)
+                return ComicParseResult.Fail("Could not find image src");
+            if (date == null)
+                return ComicParseResult.Fail("Could not find publish date");
+
+            return ComicParseResult.Succeed(new Uri(src), DateTime.Parse(date));
+        }
+    }
+}
---
+++ Comics.Tests/Core/Parsers/DilbertParserTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Comics.Core.Parsers;
+
+using NFluent;
+
+using Xunit;
+
+namespace Comics.Tests.Core.Parsers
+{
+    public class DilbertParserTests
+    {
+        [Fact]
+        public void Parse_FindsTheComicSrc()
+        {
+            var html = Fixture.Load("dilbert-2015-11-23");
+
+            var result = DilbertParser.Parse(html);
+
+            Check.That(result.ImageUri).IsEqualTo(new Uri("http://assets.amuniversal.com/041159a06560013319a6005056a9545d"));
+        }
+
+        [Fact]
+        public void Parse_FindsThePublishedDate()
+        {
+            var html = Fixture.Load("dilbert-2015-11-23");
+
+            var result = DilbertParser.Parse(html);
+
+            Check.That(result.PublishedDate).IsEqualTo(new DateTime(2015, 11, 23));
+        }
+
+        [Fact]
+        public void Parse_FailsGracefully()
+        {
+            const string html = "<html/>";
+
+            var result = DilbertParser.Parse(html);
+
+            Check.That(result.Succeeded).IsFalse();
+        }
+
+        [Fact]
+        public void Parse_ThrowsForNullInput()
+        {
+            Check.ThatCode(() => DilbertParser.Parse(null))
+                .Throws<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Parse_ThrowsForEmptyInput()
+        {
+            Check.ThatCode(() => DilbertParser.Parse(""))
+                .Throws<ArgumentException>();
+        }
+    }
+}
