09a3c6790c2a07a2b5dcd758f9de61678a61e083
Matt Burke <mburke@nerdery.com>
2015-11-24 13:03:03 -0600

    downloader

---
+++ Comics.Core/Downloaders/DilbertDownloader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Comics.Core.Downloaders
+{
+    public interface IDilbertWebClient
+    {
+        ComicDownloadResult GetComicHtml(DateTime publishedDate);
+    }
+
+    public class DilbertDownloader
+    {
+        private readonly IDilbertWebClient _client;
+
+        public DilbertDownloader(IDilbertWebClient client)
+        {
+            _client = client;
+        }
+
+        /// <summary>
+        /// Mock property
+        /// </summary>
+        public DateTime Today { get; set; } = DateTime.Today;
+
+        public IEnumerable<ComicDownloadResult> GetNewComics(DateTime lastComicDate)
+        {
+            for (var current = lastComicDate.AddDays(1).Date; current <= Today; current = current.AddDays(1).Date)
+            {
+                yield return _client.GetComicHtml(current);
+            }
+        }
+    }
+}
---
+++ Comics.Tests/Core/Downloaders/DilbertDownloaderTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using AutoMoq;
+
+using Comics.Core.Downloaders;
+
+using NFluent;
+
+using Xunit;
+
+namespace Comics.Tests.Core.Downloaders
+{
+    public  class DilbertDownloaderTests
+    {
+        [Fact]
+        public void Something()
+        {
+            var mocker = new AutoMoqer();
+            var result20151123 = new ComicDownloadResult(
+                Fixture.Load("dilbert-2015-11-23"),
+                20151123,
+                new Uri("http://dilbert.com/strip/2015-11-23"));
+
+            mocker.GetMock<IDilbertWebClient>()
+                .Setup(m => m.GetComicHtml(new DateTime(2015, 11, 23)))
+                .Returns(result20151123);
+
+            var downloader = mocker.Create<DilbertDownloader>();
+            downloader.Today = new DateTime(2015, 11, 23); // mock Today
+
+            var newComics = downloader.GetNewComics(new DateTime(2015, 11, 22));
+
+            Check.That(newComics).ContainsExactly(result20151123);
+        }
+    }
+}
