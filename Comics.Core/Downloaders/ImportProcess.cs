499ec0d55e5f5d36334de857d1c889be4511a20b
Matt Burke <mburke@nerdery.com>
2015-12-03 14:25:57 -0600

    Importer

---
+++ Comics.Core/Downloaders/ImportProcess.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Comics.Core.Persistence;
+
+namespace Comics.Core.Downloaders
+{
+    public class ComicConfigRegistry
+    {
+        public ComicConfigRegistry()
+        {
+            _entries = new List<ComicConfig>();
+        }
+
+        public void Add(ComicConfig config)
+        {
+            _entries.Add(config);
+        }
+
+        private readonly IList<ComicConfig> _entries;
+        public IEnumerable<ComicConfig> Entries => _entries;
+
+    }
+
+    public class ComicConfig
+    {
+        public ComicType ComicType { get; }
+        public IComicDownloader Downloader { get; }
+
+        public ComicConfig(ComicType comicType, IComicDownloader downloader)
+        {
+            ComicType = comicType;
+            Downloader = downloader;
+        }
+    }
+
+    public class ImportProcess
+    {
+        private readonly IComicsRepository _comics;
+        private readonly ComicConfigRegistry _registry;
+
+        public ImportProcess(IComicsRepository comics, ComicConfigRegistry registry)
+        {
+            _comics = comics;
+            _registry = registry;
+        }
+
+        public void Run()
+        {
+            foreach (var comicConfig in _registry.Entries)
+            {
+                var lastComic = _comics.GetLastImportedComic(comicConfig.ComicType);
+
+                var newComics = comicConfig.Downloader.GetNewComicsSince(lastComic);
+
+                foreach (var comic in newComics)
+                {
+                    _comics.InsertComic(comic);
+                }
+            }
+        }
+    }
+}
---
+++ Comics.Tests/Core/Downloaders/ImportProcessTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using AutoMoq;
+
+using Comics.Core.Downloaders;
+using Comics.Core.Persistence;
+
+using Moq;
+
+using Xunit;
+
+namespace Comics.Tests.Core.Downloaders
+{
+    public class ImportProcessTests
+    {
+
+
+        [Fact]
+        public void UsesRegistryToGetNewComics()
+        {
+            var mocker = new AutoMoqer();
+
+            var lastExplosmComic = new Comic();
+            var newExplosmComic1 = new Comic();
+            var newExplosmComic2 = new Comic();
+
+            var explosm = new Mock<IComicDownloader>();
+            explosm.Setup(m => m.GetNewComicsSince(lastExplosmComic))
+                .Returns(new[] { newExplosmComic1, newExplosmComic2 });
+
+            mocker.GetMock<IComicsRepository>()
+                .Setup(m => m.GetLastImportedComic(ComicType.Explosm))
+                .Returns(lastExplosmComic)
+                .Verifiable();
+
+            var registry = new ComicConfigRegistry();
+            registry.Add(new ComicConfig(ComicType.Explosm, explosm.Object));
+
+            // ReSharper disable once RedundantTypeArgumentsOfMethod
+            mocker.SetInstance<ComicConfigRegistry>(registry);
+
+            mocker.Create<ImportProcess>()
+                .Run();
+
+            mocker.GetMock<IComicsRepository>().VerifyAll();
+            mocker.GetMock<IComicsRepository>()
+                .Verify(m => m.InsertComic(newExplosmComic1), Times.Once);
+            mocker.GetMock<IComicsRepository>()
+                .Verify(m => m.InsertComic(newExplosmComic2), Times.Once);
+        }
+
+        [Fact]
+        public void UsesRegistryToGetNewComicsFromMultipleSources()
+        {
+            var mocker = new AutoMoqer();
+
+            var lastExplosmComic = new Comic();
+            var newExplosmComic1 = new Comic();
+            var newExplosmComic2 = new Comic();
+
+            var lastDilbert = new Comic();
+            var newDilbert1 = new Comic();
+            var newDilbert2 = new Comic();
+
+            var explosm = new Mock<IComicDownloader>();
+            explosm.Setup(m => m.GetNewComicsSince(lastExplosmComic))
+                .Returns(new[] { newExplosmComic1, newExplosmComic2 });
+
+            var dilbert = new Mock<IComicDownloader>();
+            dilbert.Setup(m => m.GetNewComicsSince(lastDilbert))
+                .Returns(new[] { newDilbert1, newDilbert2 });
+
+            mocker.GetMock<IComicsRepository>()
+                .Setup(m => m.GetLastImportedComic(ComicType.Explosm))
+                .Returns(lastExplosmComic)
+                .Verifiable();
+
+            mocker.GetMock<IComicsRepository>()
+               .Setup(m => m.GetLastImportedComic(ComicType.Dilbert))
+               .Returns(lastDilbert)
+               .Verifiable();
+
+            var registry = new ComicConfigRegistry();
+            registry.Add(new ComicConfig(ComicType.Explosm, explosm.Object));
+            registry.Add(new ComicConfig(ComicType.Dilbert, dilbert.Object));
+
+            // ReSharper disable once RedundantTypeArgumentsOfMethod
+            mocker.SetInstance<ComicConfigRegistry>(registry);
+
+            mocker.Create<ImportProcess>()
+                .Run();
+
+            mocker.GetMock<IComicsRepository>().VerifyAll();
+            mocker.GetMock<IComicsRepository>()
+                .Verify(m => m.InsertComic(newExplosmComic1), Times.Once);
+            mocker.GetMock<IComicsRepository>()
+                .Verify(m => m.InsertComic(newExplosmComic2), Times.Once);
+            mocker.GetMock<IComicsRepository>()
+              .Verify(m => m.InsertComic(newDilbert1), Times.Once);
+            mocker.GetMock<IComicsRepository>()
+                .Verify(m => m.InsertComic(newDilbert2), Times.Once);
+        }
+    }
+}
