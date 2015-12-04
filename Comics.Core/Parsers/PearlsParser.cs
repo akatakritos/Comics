29500ee30c3e9efd0d5d23683d1658ed9f01402e
Matt Burke <git@mattburkedev.com>
2015-12-04 08:24:32 -0600

    pearls parser

---
+++ Comics.Core/Parsers/PearlsParser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+using HtmlAgilityPack;
+
+namespace Comics.Core.Parsers
+{
+    public static class PearlsParser
+    {
+        public static ComicParseResult Parse(string html)
+        {
+            if (html == null) throw new ArgumentNullException(nameof(html));
+            if (html == "") throw new ArgumentException($"{nameof(html)} should contain some text", nameof(html));
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            var img = doc.QuerySelectorAll("img.strip")
+                .FirstOrDefault(s => !s.Attributes.Contains("width"));
+            if (img == null)
+                return ComicParseResult.Fail("Could not find strip url");
+
+            var src = img.GetAttributeValue("src", null);
+            if (src == null)
+                return ComicParseResult.Fail("img element did not have src attribute");
+
+            var meta = doc.QuerySelector("meta[name='twitter:url']");
+            if (meta == null)
+                return ComicParseResult.Fail("Could not load twitter:url attribtue");
+
+            var content = meta.GetAttributeValue("content", null);
+            if (content == null)
+                return ComicParseResult.Fail("meta tag did not have content attribute");
+
+            var dateString = content.Replace("http://www.gocomics.com/pearlsbeforeswine/", "");
+
+            var date = DateTime.ParseExact(dateString, "yyyy/MM/dd", CultureInfo.InvariantCulture);
+            var uri = new Uri(src);
+
+            return ComicParseResult.Succeed(uri, date);
+
+
+        }
+    }
+}
---
+++ Comics.Tests/Core/Parsers/PearlsParserTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Comics.Core.Parsers;
+
+using NFluent;
+
+using Xunit;
+
+namespace Comics.Tests.Core.Parsers
+{
+
+    public class PearlsParserTests
+    {
+        [Fact]
+        public void Parse_FindsTheComicSrc()
+        {
+            var html = Fixture.Load("pearls-2015-12-04");
+
+            var result = PearlsParser.Parse(html);
+
+            Check.That(result.ImageUri).IsEqualTo(new Uri("http://assets.amuniversal.com/5a7e112071f701331e71005056a9545d"));
+        }
+
+        [Fact]
+        public void Parse_FindsThePublishedDate()
+        {
+            var html = Fixture.Load("pearls-2015-12-04");
+
+            var result = PearlsParser.Parse(html);
+
+            Check.That(result.PublishedDate).IsEqualTo(new DateTime(2015, 12, 4));
+        }
+
+        [Fact]
+        public void Parse_FailsGracefully()
+        {
+            const string html = "<html/>";
+
+            var result = PearlsParser.Parse(html);
+
+            Check.That(result.Succeeded).IsFalse();
+        }
+
+        [Fact]
+        public void Parse_ThrowsForNullInput()
+        {
+            Check.ThatCode(() => PearlsParser.Parse(null))
+                .Throws<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Parse_ThrowsForEmptyInput()
+        {
+            Check.ThatCode(() => PearlsParser.Parse(""))
+                .Throws<ArgumentException>();
+        }
+    }
+}
