9ce949bb23ecf5af1726719da0bd9b522d9af8dd
Matt Burke <git@mattburkedev.com>
2015-12-19 00:22:04 -0600

    filter error to rollbar

--- Comics.Web/App_Start/FilterConfig.cs
+++ Comics.Web/App_Start/FilterConfig.cs
@@ -1,12 +1,26 @@
 ﻿using System.Web.Mvc;
 
+using RollbarSharp;
+
 namespace Comics.Web
 {
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+            filters.Add(new RollbarExceptionFilter());
             filters.Add(new HandleErrorAttribute());
+        }
+    }
+
+    public class RollbarExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext.ExceptionHandled)
+                return;
+
+            (new RollbarClient()).SendException(filterContext.Exception);
         }
     }
 }
